bc6d76fce8f0fd6d3911c99eeacad32aa9193461
Michael Bergeron <michaelbergeron@Michaels-MacBook-Pro.local>
2019-07-05 20:21:13 -0500

    added morgan

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const parser = require('body-parser');
 const cors = require('cors');
+const morgan = require('morgan')
 
 const app = express();
 
+
+app.use(morgan('combined'))
+app.use(cors());
 app.use(express.static('../dist'));
 app.use(parser.json());
-app.use(cors());
 
 app.listen(3000, ()=>console.log('listening on port 3000'));
