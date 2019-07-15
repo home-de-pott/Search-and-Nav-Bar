ba3138f71807eb0013a258fef4ddb7975ee2dc47
Michael Bergeron <michaelbergeron@Michaels-MacBook-Pro.local>
2019-07-15 11:00:35 -0500

    changed public address

--- server/server.js
+++ server/server.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const parser = require('body-parser');
 const cors = require('cors');
-// const morgan = require('morgan');
+const morgan = require('morgan');
 const path = require('path');
 const db = require('./db')
 
 const app = express();
 
 
-// app.use(morgan('combined'));
+app.use(morgan('combined'));
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../dist')));
 app.use(parser.json());
