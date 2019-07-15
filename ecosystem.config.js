ffa163d7f257cb4267bd3d4f6eab551556c3c7b7
Michael Bergeron <michaelbergeron@Michaels-MacBook-Pro.local>
2019-07-15 11:28:57 -0500

    changed database

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-18-217-166-165.us-east-2.compute.amazonaws.com/',
+      host: 'ec2-18-217-166-165.us-east-2.compute.amazonaws.com',
       key: '~/.ssh/Header-FEC.pem',
       ref: 'origin/master',
       repo: 'https://github.com/home-de-pott/Search-and-Nav-Bar.git',
