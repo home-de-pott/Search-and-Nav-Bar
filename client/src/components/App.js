151327ccebc230a33163619af623b9b06d9cf799
Michael Bergeron <michaelbergeron@Michaels-MacBook-Pro.local>
2019-07-16 11:51:29 -0500

    added scripts in componentdidmount

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -27,9 +27,9 @@ export default class App extends React.Component {
 	}
 
 	componentDidMount() {
-		appendScript("https://code.jquery.com/jquery-3.3.1.slim.min.js");
-		appendScript("https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js");
-		appendScript("https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js");
+		this.appendScript("https://code.jquery.com/jquery-3.3.1.slim.min.js");
+		this.appendScript("https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js");
+		this.appendScript("https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js");
 		window.addEventListener('addToCart', (e) => {
 			let newCart = this.state.cart;
 			let tempCart = {}
