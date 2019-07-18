d3a998b22d93c028faa574b3f6dab39386d0a785
Michael Bergeron <michaelbergeron@Michaels-MacBook-Pro.local>
2019-07-18 15:51:21 -0500

    search for cookie when a user gets to a site and keeps them logged in

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const path = require('path');
 const db = require('./db');
 const crypto = require("crypto");
-const whitelist = ['http://localhost:3000','http://localhost:3005', 'http://homedepott.us-east-2.elasticbeanstalk.com','http://localhost:3050', 'http://ec2-18-217-166-165.us-east-2.compute.amazonaws.com', 'http://homedepot.us-east-2.elasticbeanstalk.com'];
+const whitelist = ['http://homedepott.us-east-2.elasticbeanstalk.com', 'http://localhost:3000','http://localhost:3005','http://localhost:3050', 'http://ec2-18-217-166-165.us-east-2.compute.amazonaws.com', 'http://homedepot.us-east-2.elasticbeanstalk.com'];
 
 const corsOptions = {
   credentials: true,
@@ -20,8 +20,8 @@ const corsOptions = {
 
 const app = express();
 
-app.use(cookieParser())
 app.use(cors(corsOptions));
+app.use(cookieParser())
 app.use(express.static(path.join(__dirname, '../dist')));
 app.use(parser.json());
 
