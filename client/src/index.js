116190ad0c69a4bfe715527e3709ead48a34bd2b
Michael Bergeron <michaelbergeron@Michaels-MacBook-Pro.local>
2019-07-20 17:22:55 -0500

    import bootstrap css file

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,6 @@ import css from '../../dist/navBar.css';
 import React from 'react';
 import reactDOM from 'react-dom';
 import App from './components/App.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 reactDOM.render(<App />, document.getElementById('NavBar'));
