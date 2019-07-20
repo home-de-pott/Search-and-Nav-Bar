6fb5f3290a00b13438618eb99c006e4e242414c6
Michael Bergeron <michaelbergeron@Michaels-MacBook-Pro.local>
2019-07-20 11:35:22 -0500

    fixed css issue with suggest not being right below search bar as well as taking away images on small screens

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -19,11 +19,9 @@ const Header = (props) => {
   }
   const listStyle = {
     position: 'absolute',
-    display: 'block',
-    left: '275px',
+    left: '0px',
     top: '35px',
-    width: '40%',
-    minWidth: '400px',
+    width: '100%',
     'backgroundColor': 'white',
     'opacity': 1,
     'maxHeight': '400px',
@@ -45,24 +43,25 @@ const Header = (props) => {
           <img className = 'homedepotIcon' onClick = {props.homepageClick} src="https://home-de-potts.s3.us-east-2.amazonaws.com/home-depot-logo.png" height = "100px" width = "100px"/>
           <img className = 'd-none d-md-inline' src="https://home-de-potts.s3.us-east-2.amazonaws.com/location.png" height = "59px" width = "158px" />
           <div style={{display: 'inline'}}>
-            <div style={{display: 'inline'}}>
-              <span style={{display: 'inline'}} className = "input-group mb-3">
+            <div className = 'container-fluid' style={{display: 'inline'}}>
+              <span style={{position: 'relative', display: 'inline'}} className = "input-group mb-3">
                 <input style = {inputStyle} type = "text" placeholder = "What can we help you find today?"
                   onBlur = {() => setTimeout(props.loseFocusSearch, 150)}
                   onChange = {props.handleInputChange}/>
+                {props.showSuggest === true ? (
+                  <div style = {listStyle} className = "suggestMichael border border-dark">
+                    {props.suggestList.map((item) => 
+                      <div key = {item.id} className = "listclickMichael dropdown-item" onClick = {props.renderNewItem}>
+                        <span id = {item.id}>{item.name} </span>
+                        <img className = 'd-none d-md-inline' style = {{position: 'relative', float: 'right', top: '-4px'}}height = '33px' src={`https://home-de-potts.s3.us-east-2.amazonaws.com/items/${item.id}-0.jpg`} />
+                      </div>)}
+                  </div>
+                ) : (<></>)}
                 <img className="input-group-addon img-fluid p-0 m-0" style = {imgStyle} onClick = {props.renderNewItem} 
                   src="https://home-de-potts.s3.us-east-2.amazonaws.com/button.png" />
               </span>
             </div>
-              {props.showSuggest === true ? (
-                <div style = {listStyle} className = "suggestMichael border border-dark">
-                  {props.suggestList.map((item) => 
-                    <div key = {item.id} className = "listclickMichael dropdown-item" onClick = {props.renderNewItem}>
-                      <span id = {item.id}>{item.name} </span>
-                      <img style = {{position: 'relative', float: 'right', top: '-4px'}}height = '33px' src={`https://home-de-potts.s3.us-east-2.amazonaws.com/items/${item.id}-0.jpg`} />
-                    </div>)}
-                </div>
-              ) : (<></>)}
+              
             </div>
           <span style = {{padding: '30px'}}></span>
           <Login login = {props.login}
